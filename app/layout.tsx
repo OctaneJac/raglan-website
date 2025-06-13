9cd8ebd98be44b5bd5cff0c313c0365711581e0c
raglan-root <raglanroot@gmail.com>
2025-06-13 16:03:07 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,17 +19,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-W7K6EBHZQ9"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-W7K6EBHZQ9');
-        </script>
-      </head>
       <body className={inter.className}>
           <Navbar />
           <div>{children}</div>
