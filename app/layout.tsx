82905fb8798288f443a6046cd06a41575afdc791
raglan-root <raglanroot@gmail.com>
2025-06-13 15:53:13 +0500

    Update layout.tsx
    added GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +19,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-W7K6EBHZQ9"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-W7K6EBHZQ9');
+        </script>
+      </head>
       <body className={inter.className}>
           <Navbar />
           <div>{children}</div>
