5eae4a9cfa5d24d41a3b13d39ff448e6d7321a0f
sarim13 <98702591+sarim13@users.noreply.github.com>
2025-08-02 12:03:31 +0500

    Create Injector.tsx

---
+++ components/Injector.tsx
@@ -0,0 +1,28 @@
+// components/ScriptInjector.tsx
+"use client";
+
+import { useEffect } from "react";
+
+export default function scriptInjector() {
+  useEffect(() => {
+    fetch(
+      "https://3577b6869e6d.ngrok-free.app/static/tracker.js?funnel_id=e49262ca-ed87-47bb-ade6-545a020f9b86",
+      {
+        headers: {
+          "ngrok-skip-browser-warning": "true",
+        },
+      }
+    )
+      .then((res) => res.text())
+      .then((code) => {
+        const script = document.createElement("script");
+        script.textContent = code;
+        document.body.appendChild(script);
+      })
+      .catch((err) => {
+        console.error("Failed to inject tracker.js", err);
+      });
+  }, []);
+
+  return null;
+}
