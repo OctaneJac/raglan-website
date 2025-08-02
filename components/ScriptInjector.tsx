2ba503b0f3c5a717e82497a86dacb6742bf38649
sarim13 <98702591+sarim13@users.noreply.github.com>
2025-08-02 12:12:32 +0500

    Update and rename Injector.tsx to ScriptInjector.tsx

--- components/ScriptInjector.tsx
+++ components/ScriptInjector.tsx
@@ -3,7 +3,7 @@
 
 import { useEffect } from "react";
 
-export default function scriptInjector() {
+export default function ScriptInjector() {
   useEffect(() => {
     fetch(
       "https://3577b6869e6d.ngrok-free.app/static/tracker.js?funnel_id=e49262ca-ed87-47bb-ade6-545a020f9b86",
